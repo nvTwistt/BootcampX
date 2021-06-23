eabaca23970951815628173eb077eda16102dd15
nvTwistt <mtamkee@icloud.com>
2021-06-23 19:21:47 +0000

    adding paramaterized queries

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -6,14 +6,16 @@ const pool = new Pool({
   host: 'localhost',
   database: 'bootcampx'
 });
-const cohortName = [`%${process.argv[2]}%`];
+let limit = process.argv[3] || 5;
+const parameters = [`%${process.argv[2]}%`, limit];
 pool.query(`
 SELECT students.id AS id, students.name AS name, cohorts.name AS cohort
 FROM students
 JOIN cohorts ON cohort_id = cohorts.id
 WHERE cohorts.name LIKE $1
-LIMIT ${process.argv[3]};
-`, cohortName)
+ORDER BY students.id ASC
+LIMIT $2;
+`, parameters)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.name} has an id of ${user.id} and was in the ${user.cohort} cohort`);
